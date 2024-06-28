3d7c28e13fa9a763d5d906ff87cb8bbf8b1e84d2
MWPersico <willian.marcos7@hotmail.com>
2024-06-28 14:01:40 -0300

    genericservice created

---
+++ SalesWebMVC/Services/GenericService.cs
@@ -0,0 +1,54 @@
+﻿using System.CodeDom;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
+using SalesWebMVC.Services.Interfaces;
+
+namespace SalesWebMVC.Services
+{
+    public abstract class GenericService<TEntity, TKey> : IGenericService<TEntity, TKey> where TEntity : class
+    {
+        protected readonly SalesWebMVCContext _context;
+        public GenericService() { }
+
+        public GenericService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public virtual async Task<IList<TEntity>> FindAll()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public virtual async Task<TEntity> Find(TKey key)
+        {
+            if (!EntityExists(key))
+                throw new KeyNotFoundException();
+
+            return await _context.Set<TEntity>().FindAsync(key);
+        }
+
+        public virtual async Task Create(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task Delete(TKey key)
+        {
+            _context.Set<TEntity>().Remove(await Find(key));
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual bool EntityExists(TKey key)
+        {
+            return _context.Set<TEntity>().Find(key) != null;
+        }
+    }
+}
---
+++ SalesWebMVC/Services/Interfaces/IGenericService.cs
@@ -0,0 +1,13 @@
+﻿namespace SalesWebMVC.Services.Interfaces
+{
+    // Interface generica, como parâmetros de tipo uma entidade e tipo de chave primária
+    public interface IGenericService<TEntity, TKey> where TEntity : class
+    {
+        public Task<IList<TEntity>> FindAll();
+        public Task<TEntity> Find(TKey? key);
+        public Task Create(TEntity entity);
+        public Task Update(TEntity entity);
+        public Task Delete(TKey key);
+        public bool EntityExists(TKey key);
+    }
+}
