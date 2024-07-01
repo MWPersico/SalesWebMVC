cc229ac618fa31dab9e2388b02e5ecdf5e1246ca
MWPersico <willian.marcos7@hotmail.com>
2024-07-01 10:51:49 -0300

    details with navigation properties

--- SalesWebMVC/Services/SalesRecordService.cs
+++ SalesWebMVC/Services/SalesRecordService.cs
@@ -1,7 +1,17 @@
-﻿using SalesWebMVC.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
 using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Services
 {
-    public class SalesRecordService(SalesWebMVCContext context) : GenericService<SalesRecord, int?>(context);
+    public class SalesRecordService(SalesWebMVCContext context) : GenericService<SalesRecord, int?>(context)
+    {
+        public override async Task<SalesRecord> Find(int? key)
+        {
+            if (!EntityExists(key))
+                throw new KeyNotFoundException();
+
+            return await Context.SalesRecord.Include(sr=>sr.Seller).FirstOrDefaultAsync(sr=>sr.Id == key);
+        }
+    };
 }
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -1,7 +1,17 @@
-﻿using SalesWebMVC.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
 using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Services
 {
-    public class SellerService(SalesWebMVCContext context) : GenericService<Seller, int?>(context);
+    public class SellerService(SalesWebMVCContext context) : GenericService<Seller, int?>(context)
+    {
+        public override async Task<Seller> Find(int? key)
+        {
+            if (!EntityExists(key))
+                throw new KeyNotFoundException();
+
+            return await Context.Set<Seller>().Include(s => s.Department).FirstOrDefaultAsync(s=>s.Id == key);
+        }
+    };
 }
